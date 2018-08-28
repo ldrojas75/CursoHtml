e17526920e514c15b2b4b29e12845739fbc3f6b4
Leonardo Rojas <leonardorojas.ar@gmail.com>
2018-08-28 14:17:44 -0300

    avances en jQuery

--- DEV/CursoFullStack/Leccion JQ Avanzada/js/scripts.js
+++ DEV/CursoFullStack/Leccion JQ Avanzada/js/scripts.js
@@ -2,5 +2,19 @@ $(function(){
     'use strict'
     // todo el código va aquí
 
+    $('.nuestros-servicios div:first').show();
+    $('.servicios nav a:first').addClass('activo');
+
+    $('.servicios nav a').on('click', mostrarTabs);
+
+    function mostrarTabs(){
+        $('.servicios nav a').removeClass('activo');
+        $(this).addClass('activo');
+        var enlace = $(this).attr('href');
+        $('.nuestros-servicios div').hide();
+        $(enlace).show();
+        return false;
+    }
+
 
 })
